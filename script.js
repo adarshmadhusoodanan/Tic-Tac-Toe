fdfb4095bfdc75a606ff8b2bde2b29739f69fdbc
Adarsh Madhusoodanan <iadarshmadhusoodanan@gmail.com>
2024-06-24 17:58:48 +0530

    add files

---
+++ script.js
@@ -0,0 +1,96 @@
+// script.js
+const cells = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+const statusDisplay = document.getElementById('status');
+const restartBtn = document.getElementById('restartBtn');
+
+const X_CLASS = 'x';
+const O_CLASS = 'o';
+let oTurn;
+
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+startGame();
+
+restartBtn.addEventListener('click', startGame);
+
+function startGame() {
+    oTurn = false;
+    cells.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(O_CLASS);
+        cell.textContent = ''; // Clear the text content
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    setBoardHoverClass();
+    statusDisplay.innerText = `Player ${oTurn ? "O" : "X"}'s turn`;
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = oTurn ? O_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+        setBoardHoverClass();
+        statusDisplay.innerText = `Player ${oTurn ? "O" : "X"}'s turn`;
+    }
+}
+
+function endGame(draw) {
+    if (draw) {
+        statusDisplay.innerText = "Draw!";
+    } else {
+        statusDisplay.innerText = `Player ${oTurn ? "O" : "X"} Wins!`;
+    }
+    cells.forEach(cell => {
+        cell.removeEventListener('click', handleClick);
+    });
+}
+
+function isDraw() {
+    return [...cells].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
+    });
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+    cell.textContent = currentClass.toUpperCase(); // Set text content to 'X' or 'O'
+}
+
+function swapTurns() {
+    oTurn = !oTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(O_CLASS);
+    if (oTurn) {
+        board.classList.add(O_CLASS);
+    } else {
+        board.classList.add(X_CLASS);
+    }
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cells[index].classList.contains(currentClass);
+        });
+    });
+}
